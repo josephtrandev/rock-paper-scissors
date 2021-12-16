390d8608b2efb019a153f06420deb8f8210cf3c8
Joseph <56847381+josephtrandev@users.noreply.github.com>
2021-12-15 20:44:45 -0600

    Added every condition for each choice

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,21 +5,55 @@ function computerPlay() {
     return pick.toLowerCase();
 }
 
-//console.log(computerPlay());
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        alert('Draw! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' and '+ computerSelection.toLowerCase() + ' are the same');
-    }
-    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        alert('You win! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' beats ' + computerSelection.toLowerCase());
-    }
-    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        alert('You lose! ' + computerSelection + ' beats ' + playerSelection.toLowerCase());
-    }
-
+function playerPlay() {
+	let playerSelection = prompt("Pick rock, paper, or scissors.");
+	
+	return playerSelection.toLowerCase();
 }
 
-let playerSelection = prompt("Pick rock, paper, or scissors.").toLowerCase();
+//if (choices1.indexOf(playerSelection) !== -1) {
+//		let playerSelection = prompt("Invalid input. \nPick rock, paper, or scissors.");
+//}
+
+function playRound(playerSelection, computerSelection) {	
+    if (playerSelection == computerSelection) {
+        alert('Draw! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' and '+ computerSelection + ' are the same');
+    }
+    else if (computerSelection == 'rock') {
+		switch(playerSelection) {
+			case 'paper':
+				alert('You win! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' beats ' + computerSelection);
+				break;
+			case 'scissors':
+				alert('You lose! ' + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)) + ' beats ' + playerSelection);
+				break;
+		}
+    }
+	else if (computerSelection == 'paper') {
+        switch(playerSelection) {
+			case 'scissors':
+				alert('You win! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' beats ' + computerSelection);
+				break;
+			case 'rock':
+				alert('You lose! ' + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)) + ' beats ' + playerSelection);
+				break;
+		}
+    }
+	else if (computerSelection == 'scissors') {
+        switch(playerSelection) {
+			case 'rock':
+				alert('You win! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' beats ' + computerSelection);
+				break;
+			case 'paper':
+				alert('You lose! ' + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)) + ' beats ' + playerSelection);
+				break;
+		}
+    }
+}
+
 const computerSelection = computerPlay();
+//console.log(computerSelection);
+const playerSelection = playerPlay();
+//console.log(playerSelection);
+
 console.log(playRound(playerSelection, computerSelection));
