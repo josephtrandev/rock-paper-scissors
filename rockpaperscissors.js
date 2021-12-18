b323e2b8ea1c80c6f2bf6568b933a603761ee4a2
Joseph <56847381+josephtrandev@users.noreply.github.com>
2021-12-17 19:54:45 -0600

    Implemented best of X amount of rounds game logic

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -12,49 +12,112 @@ function playerPlay() {
 	return pick.toLowerCase();
 }
 
-function playRound(playerSelection, computerSelection) {	
+function playRound(playerSelection, computerSelection) {
+	let win = ('You win! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' beats ' + computerSelection);
+	let lose = ('You lose! ' + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)) + ' beats ' + playerSelection);
+	let draw = ('Draw! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' and '+ computerSelection + ' are the same');
+	
     if (playerSelection == computerSelection) {
-		let result = ('Draw! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' and '+ computerSelection + ' are the same');
-		
+		result = draw;
+		return result;
 	}
-    else if (computerSelection == 'rock') {
-		switch(playerSelection) {
+    else if (playerSelection == 'rock') {
+		switch(computerSelection) {
 			case 'paper':
-				result = ('You win! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' beats ' + computerSelection);
+				result = lose;
+				return result;
 			case 'scissors':
-				result = ('You lose! ' + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)) + ' beats ' + playerSelection);
+				result = win;
+				return result;
 		}
     }
-	else if (computerSelection == 'paper') {
-        switch(playerSelection) {
+	else if (playerSelection == 'paper') {
+        switch(computerSelection) {
 			case 'scissors':
-				result = ('You win! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' beats ' + computerSelection);
+				result = lose;
+				return result;
 			case 'rock':
-				result = ('You lose! ' + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)) + ' beats ' + playerSelection);
+				result = win;
+				return result;
 		}
     }
-	else if (computerSelection == 'scissors') {
-        switch(playerSelection) {
+	else if (playerSelection == 'scissors') {
+        switch(computerSelection) {
 			case 'rock':
-				result = ('You win! ' + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + ' beats ' + computerSelection);
+				result = lose;
+				return result;
 			case 'paper':
-				result = ('You lose! ' + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)) + ' beats ' + playerSelection);
+				result = win;
+				return result;
 		}
     }
 	
-	alert(result);
-	return result;
+	
 }
 
-const computerSelection = computerPlay();
-//console.log(computerSelection);
-playerSelection = playerPlay();
-//console.log(playerSelection);
+function game() {
+	var currRound = 1
+	var playUntil = 5;
+	var playerWins = 0;
+	var computerWins = 0;
+	var winner = "";
+	
+	while (true) {
+		if (playerWins == Math.floor((playUntil/2))+1) {
+			winner = "PLAYER WINS";
+			console.log(winner);
+			return winner;
+			break;
+		}
+		else if (computerWins == Math.floor((playUntil/2))+1) {
+			winner = "COMPUTER WINS";
+			console.log(winner);
+			return winner;
+			break;
+		} 
+		
+		console.log("Round " + currRound);
+		computerSelection = computerPlay();
+		console.log("Computer picks " + computerSelection);
+		playerSelection = playerPlay();
+		console.log("Player picks " + playerSelection);
+		
+		while (choices.indexOf(playerSelection.toLowerCase()) < 0) {
+			alert('Invalid input.');
+			playerSelection = playerPlay();
+			console.log("Player picks " + playerSelection);
+		}
+		
+		playRound(playerSelection, computerSelection);
+		//alert(result);
+		console.log(playRound(playerSelection, computerSelection));
 
-if (choices.indexOf(playerSelection.toLowerCase()) < 0) {
-	alert('Invalid input.');
-	playerSelection = playerPlay();
-	//console.log(playerSelection);
+		if (typeof result !== undefined) {
+			if (result.includes("win")) {
+				playerWins++;
+				currRound++;
+				console.log("Player wins: " + playerWins);
+				console.log("Computer wins: " + computerWins);
+				console.log(" ");
+			}
+			else if (result.includes("lose")) {
+				computerWins++;
+				currRound++;
+				console.log("Player wins: " + playerWins);
+				console.log("Computer wins: " + computerWins);
+				console.log(" ");
+			}
+			else if (result.includes("same")) {
+				console.log("Player wins: " + playerWins);
+				console.log("Computer wins: " + computerWins);
+				console.log(" ");
+			}
+		}
+		
+	}
+	
+	
+	
 }
 
-//console.log(playRound(playerSelection, computerSelection));
+game();
